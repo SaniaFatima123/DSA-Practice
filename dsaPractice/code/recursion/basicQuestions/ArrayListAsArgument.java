161f6fbcaab9b0f4a9a69bf050d67bc3084d1f56
SaniaFatima123 <fatimasania648@gmail.com>
2023-09-23 22:34:52 +0530

    changes in comment

--- dsaPractice/code/recursion/basicQuestions/ArrayListAsArgument.java
+++ dsaPractice/code/recursion/basicQuestions/ArrayListAsArgument.java
@@ -8,8 +8,7 @@ public class ArrayListAsArgument {
         System.out.println(findAllIndex(arr, 4,0,new ArrayList<>()));
     }
     //if you are passing arraylist as parameter in recursion call it will reflect changes in the same object
-    //if you are creating arraylist in the body of the method during recursion call, it will stay only for that particular call,
-    // and it will change for the next recursion call everytime
+
     static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list){
         if(index==arr.length){
             return list;
--- dsaPractice/code/recursion/basicQuestions/ArrayListInBody.java
+++ dsaPractice/code/recursion/basicQuestions/ArrayListInBody.java
@@ -7,6 +7,8 @@ public class ArrayListInBody {
         int[] arr = {2,43,76,4,7,5,4,5};
         System.out.println(findAllIndex(arr, 4,0));
     }
+    //if you are creating arraylist in the body of the method during recursion call, it will stay only for that particular call,
+    // and it will change for the next recursion call everytime
     static ArrayList<Integer> findAllIndex(int[] arr, int target, int index){
         ArrayList<Integer> list = new ArrayList<>();
         if(index == arr.length){
