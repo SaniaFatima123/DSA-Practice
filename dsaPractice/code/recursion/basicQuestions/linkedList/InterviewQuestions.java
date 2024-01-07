4314da4c8678e50fc56a6b8a32b3bb59110a92e0
SaniaFatima123 <fatimasania648@gmail.com>
2024-01-07 23:44:43 +0530

    interview question added

--- dsaPractice/code/recursion/basicQuestions/linkedList/InterviewQuestions.java
+++ dsaPractice/code/recursion/basicQuestions/linkedList/InterviewQuestions.java
@@ -77,6 +77,17 @@ public class InterviewQuestions {
         }
         return s;
     }
+//    https://leetcode.com/problems/middle-of-the-linked-list/submissions/
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
     class ListNode {
         int val;
         ListNode next;
