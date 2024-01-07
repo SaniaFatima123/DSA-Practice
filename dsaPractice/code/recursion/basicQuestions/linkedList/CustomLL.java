d945abbf66f4b4281306472cae2d8584c08f6a0e
SaniaFatima123 <fatimasania648@gmail.com>
2024-01-08 01:46:31 +0530

    merge sort and bubble sort questions added in LL

--- dsaPractice/code/recursion/basicQuestions/linkedList/CustomLL.java
+++ dsaPractice/code/recursion/basicQuestions/linkedList/CustomLL.java
@@ -174,6 +174,39 @@ public class CustomLL {
         }
         return ans;
     }
+    private void bubbleSort(int row, int col) {
+        if (row == 0) {
+            return;
+        }
+
+        if (col < row) {
+            Node first = get(col);
+            Node second = get(col + 1);
+
+            if (first.value > second.value) {
+                // swap
+                if (first == head) {
+                    head = second;
+                    first.next = second.next;
+                    second.next = first;
+                } else if (second == tail) {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    tail = first;
+                    first.next = null;
+                    second.next = tail;
+                } else {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    first.next = second.next;
+                    second.next = first;
+                }
+            }
+            bubbleSort(row, col + 1);
+        } else {
+            bubbleSort(row - 1, 0);
+        }
+    }
     public void display()
     {
         Node temp = head;
